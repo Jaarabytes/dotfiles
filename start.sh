129cdfc289047acae3c1bd76e714d6babe031eb2
Jaarabytes <xavierandole78@gmail.com>
2024-07-16 13:39:36 +0300

    feat: add pacman config
    Add installation for pacman and arch based distros

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# function to install packages using pacman (arch based distros)
+function install_pacman_packages() {
+  local packages=("$@")
+  for package in "${packages[@]}"; do
+    echo "Installing $package..."
+    if sudo pacman -Sy "$package"; then
+      echo "Successfully installed $package"
+    else
+      echo "Error installing $package"
+    fi
+  done
+}
+
 # Function to install packages using apt
 function install_apt_packages() {
   local packages=("$@")
@@ -31,7 +44,7 @@ function install_snap_packages() {
   local packages=("$@")
   for package in "${packages[@]}"; do
     echo "Installing $package..."
-    if snap install "$package"; then
+    if sudo snap install "$package"; then
       echo "Successfully installed $package"
     else
       echo "Error installing $package"
@@ -43,16 +56,24 @@ function install_snap_packages() {
 read -p "Update system first (y/N)? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sudo apt update -y && sudo apt upgrade -y
-  echo "Updated system"
+  read -p "Is your system debian based/ arch based (d/A)? " -n 1 -r 
+  echo
+  if [[ $REPLY =~ ^[dD]$ ]]; then
+    sudo apt update -y && sudo apt upgrade -y
+    echo "Updated system"
+  elif [[ $REPLY =~ ^[aA]$ ]]; then
+    sudo pacman -Sy
+    echo "Updated system"
+  fi
 fi
 
 # Define package lists
-apt_packages=(ffmpeg tree eza bat htop iftop iostat netstat make curl wget grep sed awk nvim git docker python3-pip)
+apt_packages=(ffmpeg kubernetes-client tree eza bat htop iftop iostat netstat make curl wget grep sed awk nvim git docker python3-pip)
 npm_packages=(vercel@latest netlify-cli svelte-language-server typescript)
 snap_packages=(code obsidian slack telegram-desktop discord spotify)
 
 # Install packages
+install_pacman_packages "${apt_packages[@]}"
 install_apt_packages "${apt_packages[@]}"
 install_npm_packages "${npm_packages[@]}"
 install_snap_packages "${snap_packages[@]}"
